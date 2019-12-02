712819a5daee167e4d93f4941f94f704a2c3c783
Jimmy Wang <wangziming1022@foxmail.com>
2019-12-02 21:37:01 +0800

    增加登录页面；增加导航栏

---
+++ web/src/components/LoginForm.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-tabs type="border-card" stretch>
+    <el-tab-pane label="登录">
+      <h1>登录</h1>
+      <el-input class="input" v-model="username" placeholder="请输入用户名">
+        <div slot="prepend" class="input-prepend">用户名</div>
+      </el-input>
+      <el-input class="input" v-model="password" placeholder="请输入密码" type="password">
+        <div slot="prepend" class="input-prepend">密码</div>
+      </el-input>
+      <div></div>
+      <el-button class="button" type="primary" @click="login">登录</el-button>
+    </el-tab-pane>
+    <el-tab-pane label="注册">
+      <h1>注册</h1>
+      <el-input class="input" v-model="username" placeholder="请输入用户名">
+        <div slot="prepend" class="input-prepend">用户名</div>
+      </el-input>
+      <el-input class="input" v-model="password" placeholder="请输入密码" type="password">
+        <div slot="prepend" class="input-prepend">密码</div>
+      </el-input>
+      <el-input class="input" v-model="repeatPassword" placeholder="请再次输入密码" type="password">
+        <div slot="prepend" class="input-prepend">密码</div>
+      </el-input>
+      <div></div>
+      <el-button class="button" type="primary" @click="register">注册</el-button>
+    </el-tab-pane>
+  </el-tabs>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+
+  private username: string = '';
+  private password: string = '';
+  private repeatPassword: string = '';
+
+  private async login() {
+    console.log(this.username, this.password);
+  }
+
+  private async register() {
+    console.log(this.username, this.password)
+  }
+
+}
+
+</script>
+
+<style lang="css" scope>
+  .input {
+    margin-top:15px;
+    margin-bottom: 10px;
+    width: 80% !important;
+  }
+
+  .input-prepend {
+    width: 80px;
+  }
+
+  .button {
+    margin-top: 10px !important;
+    margin-bottom: 10px !important;
+  }
+
+</style>
---
+++ web/src/views/Main.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <el-menu :default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <li class="el-menu-item title">活动召集系统</li>
+    <el-menu-item index="1">处理中心</el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">我的工作台</template>
+      <el-menu-item index="2-1">选项1</el-menu-item>
+      <el-menu-item index="2-2">选项2</el-menu-item>
+      <el-menu-item index="2-3">选项3</el-menu-item>
+      <el-submenu index="2-4">
+        <template slot="title">选项4</template>
+        <el-menu-item index="2-4-1">选项1</el-menu-item>
+        <el-menu-item index="2-4-2">选项2</el-menu-item>
+        <el-menu-item index="2-4-3">选项3</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-menu-item index="3" disabled>消息中心</el-menu-item>
+    <li class="el-menu-item right">用户</li>
+  </el-menu>
+  <router-view></router-view>
+</div>
+</template>
+
+<style lang="css" scope>
+  .title {
+    font-size: 25px !important;
+    color: #555 !important;
+  }
+
+  .right {
+    float: right !important;
+    margin-right: 50px !important;
+  }
+</style>
