40b0fecdbf63a0ba9b39e36fe6c542c6ed791100
Green-Cherry <2423964104@qq.com>
2019-12-30 12:40:19 +0800

    <修改> 修改调用算法

--- sever/src/main/java/cn/edu/sjtu/bpmproject/server/service/impl/CommentServiceImpl.java
+++ sever/src/main/java/cn/edu/sjtu/bpmproject/server/service/impl/CommentServiceImpl.java
@@ -4,12 +4,14 @@ import cn.edu.sjtu.bpmproject.server.dao.CommentDao;
 import cn.edu.sjtu.bpmproject.server.entity.Comment;
 import cn.edu.sjtu.bpmproject.server.entity.PushMessage;
 import cn.edu.sjtu.bpmproject.server.service.CommentService;
+import cn.edu.sjtu.bpmproject.server.util.BertAPIUtil;
 import cn.edu.sjtu.bpmproject.server.util.NlpAPIUtil;
 import cn.edu.sjtu.bpmproject.server.util.TimeUtil;
 import com.baidu.aip.nlp.AipNlp;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,8 +19,8 @@ import java.util.List;
 @Component
 public class CommentServiceImpl implements CommentService{
 
-    private AipNlp client = new AipNlp(NlpAPIUtil.APP_ID, NlpAPIUtil.API_KEY, NlpAPIUtil.SECRET_KEY);
-
+    @Autowired
+    private RestTemplate restTemplate;
     @Autowired
     private CommentDao commentDao;
 
@@ -29,7 +31,7 @@ public class CommentServiceImpl implements CommentService{
         HashMap<String, Object> options = new HashMap<String, Object>();
 
         // 情感倾向分析
-        JSONObject res = client.sentimentClassify(text, options);
+        JSONObject res = restTemplate.postForObject(BertAPIUtil.URL,pushMessage, JSONObject.class);
         res= res.getJSONArray("items").getJSONObject(0);
 
         double positive_prob=(double)res.get("positive_prob");
---
+++ sever/src/main/java/cn/edu/sjtu/bpmproject/server/util/BertAPIUtil.java
@@ -0,0 +1,6 @@
+package cn.edu.sjtu.bpmproject.server.util;
+
+public class BertAPIUtil {
+
+    public static final String URL = "10.0.0.52:8080/predict";
+}
